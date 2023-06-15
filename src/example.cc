03d953d9de3f2f2ea2c9ac20365dd6cb5705da64
dream727 <dream77070@163.com>
2023-06-15 12:06:35 +0800

    Targets接入Options后有bug，现在得用一个ErrorList来解决

--- src/example.cc
+++ src/example.cc
@@ -9,50 +9,21 @@ import coc;
 using namespace std;
 using namespace coc;
 void test(Getter g){
-    if(g.is_empty){
-        cout<<"empty\n";
-        return;
-    }
-    cout<<"-----------------------------------\n";
-    cout<<"options test:\n";
-    for(auto iter:g.get_opt()->get_list()){
-        cout<<iter->name<<"\n";
-    }
-    cout<<"-----------------------------------\n";
-    cout<<"arguments test\n";
-    cout<<"arguments bool test:"<<g.get_arg()->getBool("BOOL",true)<<"\n";
-    cout<<"arguments int test:"<<g.get_arg()->getInt("INT",-1)<<"\n";
-    cout<<"arguments string test:"<<g.get_arg()->getString("STRING","string")<<"\n";
-    cout<<"arguments char test:"<<g.get_arg()->getChar("CHAR",'t')<<"\n";
-    cout<<"arguments float test:"<<g.get_arg()->getFloat("FLOAT",-1.1f)<<"\n";
-
-    cout<<"-----------------------------------\n";
-    cout<<"value test:\n";
-
-    cout<<"-----------------------------------\n";
     cout<<"targets test:\n";
-    cout<<g.get_tar()->at(0,"default");
+    cout<<g.get_tar()->at(g.get_arg()->getInt("pos",3),"default")<<'\n';
+    cout<<g.get_tar()->size_f()<<'\t'<<g.get_tar()->size("option");
 }
 void global_action(Getter g){
 
 }
 
-void rstringsss(string&& rv){
-
-}
-
-void printString(string str){
-    rstringsss(std::move(str));
-    cout<<str<<endl;
-}
 int main(int argc,char**argv) {
-    printString("hello world");
     auto *config = new ParserConfig;
     Log *log = new Log;
     Parser parser(config, log);
     IHelpFunc *hf=new HelpFunc(config,&parser);
+    parser.addAction("test","this is a test",test)
+            ->addOption("option","",2,'o');
+    parser.addArgument("pos","string","this is a arg");
     return parser.run(argc, argv);
-    //target/coc test -fs -DBOOL=false -DINT=2233 -DCHAR=c -DSTRING=s -DFLOAT=22.33 argv argv
-    //target/coc test --foption -s -DBOOL=false -DINT=2233 -DCHAR=c -DSTRING=s -DFLOAT=22.33 argv argv
-    //target/coc test --foption --soption -DBOOL=false -DINT=2233 -DCHAR=c -DSTRING=s -DFLOAT=22.33 argv argv
 }
